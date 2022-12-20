fb1d8985531c1b0aff270a87fb9e0551d2553266
mateusfranca97 <mateus10gomes@gmail.com>
2022-12-19 21:11:59 -0300

    First Commit

---
+++ HOCrud/Operador.cs
@@ -0,0 +1,83 @@
+﻿namespace HOCrud
+{
+    internal class Operador
+    {
+        private string _matricula;
+        private string _name;
+        private string _cep;
+        private string _telefone;
+        private string _operacao;
+
+        public override string ToString()
+        {
+            return $"\nMatricula: {_matricula}\nNome: {_name}\nCEP: {_cep}\nTelefone: {_telefone}\nOperação: {_operacao}";
+        }
+
+        public Operador()
+        {
+
+        }
+
+        public Operador(string matricula, string name, string cep, string telefone, string operacao)
+        {
+            Matricula = matricula;
+            Name = name;
+            Cep = cep;
+            Telefone = telefone;
+            Operacao= operacao;
+        }
+
+        public string Matricula 
+        { 
+            get { return _matricula; }
+            set
+            {
+                if (value.Length != 6)
+                {
+                    Console.WriteLine($"Matricula {value} Invalida.\nMatricula deve conter 6 digitos: Ex.: 123456");
+
+                } else
+                {
+                    _matricula = value;
+                    Console.WriteLine($"Usuario {value} Cadastrado.");
+                }
+                
+            }
+        }
+        public string Name 
+        { 
+            get 
+            { 
+                return _name; 
+            }
+            set 
+            {
+                if (value.Length == 0)
+                {
+                    Console.WriteLine("Campo nome VAZIO.");
+                }
+                else
+                {
+                    _name = value;
+                }
+            } 
+        }
+        public string Cep 
+        { 
+            get { return _cep; }
+            set 
+            {
+                if (value.Length != 8)
+                {
+                    Console.WriteLine("CEP Invalido.");
+                }
+                else 
+                { 
+                    _cep = value; 
+                }
+            } 
+        }
+        public string Telefone { get => _telefone; set => _telefone = value; }
+        public string Operacao { get => _operacao; set => _operacao = value; }
+    }
+}
---
+++ HOCrud/Program.cs
@@ -0,0 +1,68 @@
+﻿using HOCrud;
+using System.Xml;
+using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        List<Operador> operadors= new List<Operador>();
+        Operador operador1 = new Operador("123456", "Maria Vitoria", "12345678", "8191234-9876", "Treinamento");
+        operadors.Add(operador1);
+        int choose = null;
+        while (choose != 0)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("0 - Sair do programa.");
+            Console.WriteLine("1 - Listar de Cadastro");
+            Console.WriteLine("2 - Adicionar");
+            Console.WriteLine("3 - Remover");
+            Console.Write("Selecione uma Opção: ");
+            choose = int.Parse(Console.ReadLine());
+            switch (choose)
+            {
+                case 0:
+                    Console.WriteLine("Sair do programa.");
+                    choose = 0;
+                    break;
+                case 1:
+                    Console.WriteLine("Listar de Cadastro");
+                    if (operadors.Capacity == 0 )
+                    {
+                        Console.WriteLine("Lista está VAZIA.");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        foreach (var item in operadors)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                    
+                    break;
+                case 2:
+                    Operador operador = new Operador();
+                    Console.WriteLine("Cadastrar");
+                    Console.Write("Matricula: ");
+                    operador.Matricula = Console.ReadLine();
+                    Console.Write("Nome: ");
+                    operador.Name = Console.ReadLine();
+                    Console.Write("CEP: ");
+                    operador.Cep = Console.ReadLine();
+                    Console.Write("Telefone: ");
+                    operador.Telefone = Console.ReadLine();
+                    Console.Write("Operação: ");
+                    operador.Operacao = Console.ReadLine();
+                    operadors.Add(operador);
+                    Console.WriteLine("Usuario Cadastrado");
+                    break;
+                case 3:
+                    Console.WriteLine("Remover");
+                    break;
+            }
+
+        }
+    }
+}
